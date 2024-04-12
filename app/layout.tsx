b2f7fd1781925addb5b6375090a0a68761abcf2a
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2024-04-13 02:07:13 +0530

    fixed: UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" >
+      <meta name="fit" content="width=device-width, initial-scale=1"/>
+      <body className={inter.className}>
+       
+        <div className="">{children}</div>
+        </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,36 +1,42 @@
-// page.js
-"use client"
+"use client";
 
-import { useState } from 'react';
-import axios from 'axios'; 
+import { useState } from "react";
+import axios from "axios";
 
 export default function Page() {
-  const [url, setUrl] = useState('');
-  const [htmlContent, setHtmlContent] = useState('');
+  const [url, setUrl] = useState("");
+  const [htmlContent, setHtmlContent] = useState("");
 
   const fetchData = async () => {
     try {
-      const response = await axios.post('/api/scrape', { url }); // Send URL in the request body using axios
+      const response = await axios.post("/api/scrape", { url }); // Send URL in the request body using axios
       setHtmlContent(response.data); // Assuming the backend returns scraped HTML data
     } catch (error) {
-      console.error('Error fetching data:', error);
-      setHtmlContent('Failed to fetch data. Check the console for details.'); // Inform user about error
+      console.error("Error fetching data:", error);
+      setHtmlContent("Failed to fetch data. Check the console for details."); // Inform user about error
     }
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="Enter URL"
-        className='text-red-800'
-      />
-      <button onClick={fetchData}>Fetch Data</button>
-      <div>
-        <h2>HTML Content:</h2>
-        <pre>{htmlContent}</pre>
+    <div className="bg-gray-950 max-h-screen max-w-full p-10 ">
+      <div className="flex flex-col space-y-4">
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Enter URL"
+          className="rounded-md border pb-3 bg-gray-800 text-white border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        />
+        <button
+          onClick={fetchData}
+          className="inline-flex items-center px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Fetch Data
+        </button>
+      </div>
+      <div className="mt-6">
+        <h2 className="pb-2">HTML Content:</h2>
+        <textarea className="rounded-md text-wrap bg-gray-800  p-4 w-full min-h-screen text-white" value={htmlContent} />
       </div>
     </div>
   );
